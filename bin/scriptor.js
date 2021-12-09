8109451647facc3ed6dcdb1c62f84382d3a7f00f
Johannes Kiesel <johannes.kiesel@uni-weimar.de>
2021-12-09 14:05:24 +0100

    started chain config

--- bin/scriptor.js
+++ bin/scriptor.js
@@ -6,10 +6,11 @@ const cli = require('../lib/cli.js');
 const docker = require('../lib/docker.js');
 const log = require('../lib/log.js');
 
-const options = cli.parse(forDocker = true)
-const inputDirectory = cli.getInputDirectory(options);
-const outputDirectory = cli.getOutputDirectory(options);
-cli.getRunOptions(options); // sanity check
+////////////////////////////////////////////////////////////////////////////////
+// MAIN
+////////////////////////////////////////////////////////////////////////////////
 
-const chainable = docker.run(inputDirectory, outputDirectory, options);
+const forDocker = true;
+const options = cli.parse(forDocker);
+docker.run(options);
 
